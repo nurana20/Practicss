84bc7f0e0b9e3c54002630aa86a1b86a1b80565e
Nurane Ismayilzade <nuraneai@code.edu.az>
2024-02-07 19:10:09 +0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,75 @@
+"use strict";
+let word="    Azerbaycan";
+// console.log(word.length);
+// for (let i = 0; i < word.length; i++) {
+//     console.log(word[i]);
+// }
+// console.log(word.toLocaleLowerCase());
+// console.log(word.toUpperCase());
+// console.log(word.trim());
+// console.log(word.charAt(8));
+// console.log(word.charCodeAt(9));
+// let result=word.concat(" salam")
+// console.log(result);
+// console.log(word.slice(6,12));
+// console.log(word.substring(6,12));
+// let txt="salam nurane necesen?";
+// console.log(txt);
+// console.log(txt.substring(0,12));
+// let res=txt.substring(0,11)+" ...";
+// console.log(res);
+// let ress=word.replace("A","a")
+// console.log(ress);
+// let resss=word.indexOf("z")
+// console.log(resss);
+// console.log(word.lastIndexOf("a"));
+// console.log(word.indexOf("a"));
+// console.log(word.replaceAll("a","b"));
+// console.log(word.replace("a","b"));
+// let res=word.split();
+// console.log(word);
+// let woord="Azerbaycan salam necesen?";
+// let res=woord.split(" ")
+// console.log(res);
+// console.log(res[0]);
+// console.log(woord.startsWith(" "));
+// let ages=[22,44,50,16]
+// let res=ages.toString();
+// console.log(res);
+// console.log(ages.at(0));
+// const fruits=["Banana","Orange","Apple","Mango"]
+// console.log(fruits.join(" * "));
+// fruits.pop();
+// console.log(fruits);
+// fruits.push("test")
+// console.log(fruits);
+// fruits.shift()
+// console.log(fruits);
+// fruits.unshift("salam")
+// console.log(fruits);
+// const fruits=["Banana","Orange","Apple","Mango"]
+// let res=fruits.filter(m=>m.includes("n"))
+// console.log(res);
+// let ress=fruits.filter(m=>m.includes("Orange"))
+// console.log(ress);
+// fruits=fruits.filter(m=>!m.includes("Orange"))
+// console.log(fruits);
+// console.log(fruits.includes("Banana"));
+// let result=fruits.find(m=>m.startsWith("n"))
+// console.log(result);
+// fruits.forEach(fruit=>{
+//     console.log(fruit);
+// })
+// fruits.forEach((fruit,x)=>{
+//     console.log(fruit+ "-" +x);
+// })
+// console.log(fruits);
+// fruits.forEach((fruit,i)=>{
+//     fruits[i]=fruit+ "P418";
+// })
+// console.log(fruits);
+// let res=fruits.map(m=>{
+//     return m + "P418";
+// })
+// console.log(res);
+// console.log(fruits);
